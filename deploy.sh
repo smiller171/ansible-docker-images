0b9a3b71703b7d2f3c6866ae36db1670c6c241b5
Scott Miller <smiller@contentanalyst.com>
2015-08-31 12:41:08 -0400

    add login step to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
 docker push scottmiller171/ubuntu-ansible:latest
 docker push scottmiller171/debian-ansible:latest
 docker push scottmiller171/centos-ansible:latest
