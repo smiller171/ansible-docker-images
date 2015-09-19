a9873299abe1a0f04f303d80043612eec31e69ee
Scott Miller <scott.miller171@gmail.com>
2015-09-19 13:46:57 -0400

    build docker images in Docker hub instead of Travis, in order to allow automatic building when Base image is updated

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push scottmiller171/ubuntu-ansible:latest
-docker push scottmiller171/debian-ansible:latest
-docker push scottmiller171/centos-ansible:latest
-docker push scottmiller171/opensuse-ansible:latest
